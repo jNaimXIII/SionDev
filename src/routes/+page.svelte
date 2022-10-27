f0979cded59f9f7fe749edfb87ba1a0237d1954a
Jannatin Naim <soup@jannatinnaim.com>
2022-10-27 15:14:19 +0600

    update hero styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import SiteHeader from "$lib/components/global/SiteHeader.svelte";
   import HeroBackgroundGrid from "./_HeroBackgroundGrid.svelte";
+  import HeroBackgroundParticles from "./_HeroBackgroundParticles.svelte";
 </script>
 
 <svetle:head>
@@ -10,11 +11,16 @@
 <main>
   <div class="hero">
     <HeroBackgroundGrid />
+    <HeroBackgroundParticles />
 
     <SiteHeader />
 
     <div class="content">
-      <h1 class="site-heading">siondev ag</h1>
+      <h1 class="site-heading glitch">
+        <span>siondev ag</span>
+        siondev ag
+        <span>siondev ag</span>
+      </h1>
 
       <div class="catchphrase">
         <p class="punchline">
@@ -45,6 +51,8 @@
   }
 
   .hero {
+    position: relative;
+    overflow: hidden;
     min-height: 100vh;
     padding: 100px;
 
@@ -76,15 +84,20 @@
       }
 
       .site-heading {
-        font-size: 128px;
+        font-size: 108px;
         position: relative;
+        text-transform: uppercase;
 
         @media (max-width: 968px) {
           font-size: 96px;
         }
 
-        @media (max-width: 532px) {
+        @media (max-width: 592px) {
           font-size: 66px;
+        }
+
+        @media (max-width: 480px) {
+          font-size: 56px;
         }
 
         &::before {
@@ -102,6 +115,55 @@
           }
 
           z-index: -1;
+        }
+      }
+
+      .glitch {
+        text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff, 0.025em 0.04em 0 #fffc00;
+        animation: glitch 725ms infinite;
+      }
+
+      .glitch span {
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+
+      .glitch span:first-child {
+        animation: glitch 500ms infinite;
+        clip-path: polygon(0 0, 100% 0, 100% 35%, 0 35%);
+        transform: translate(-0.04em, -0.03em);
+        opacity: 0.75;
+      }
+
+      .glitch span:last-child {
+        animation: glitch 375ms infinite;
+        clip-path: polygon(0 65%, 100% 65%, 100% 100%, 0 100%);
+        transform: translate(0.04em, 0.03em);
+        opacity: 0.75;
+      }
+
+      @keyframes glitch {
+        0% {
+          text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff, 0.025em 0.04em 0 #fffc00;
+        }
+        15% {
+          text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff, 0.025em 0.04em 0 #fffc00;
+        }
+        16% {
+          text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff, -0.05em -0.05em 0 #fffc00;
+        }
+        49% {
+          text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff, -0.05em -0.05em 0 #fffc00;
+        }
+        50% {
+          text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff, 0 -0.04em 0 #fffc00;
+        }
+        99% {
+          text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff, 0 -0.04em 0 #fffc00;
+        }
+        100% {
+          text-shadow: -0.05em 0 0 #00fffc, -0.025em -0.04em 0 #fc00ff, -0.04em -0.025em 0 #fffc00;
         }
       }
 
@@ -162,6 +224,8 @@
   }
 
   .exposure {
+    background: rgba(109, 30, 30, 0.03);
+
     .content {
       padding: 152px 244px;
 
@@ -214,6 +278,7 @@
 
       &-start {
         background: linear-gradient(270deg, rgba(255, 255, 255, 0.2) 0%, rgba(116, 0, 140, 0.2) 100%);
+
         position: relative;
 
         &::before {
---
+++ src/routes/_HeroBackgroundParticles.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  const particleCount = 25;
+</script>
+
+{#each Array(particleCount) as _}
+  <div class="firefly" />
+{/each}
+
+<style lang="sass">
+  @use "sass:math"
+
+  $quantity: 25
+
+  .firefly
+    position: absolute
+    left: 50%
+    top: 50%
+    width: 0.4vw
+    height: 0.4vw
+    margin: -0.2vw 0 0 9.8vw
+    animation: ease 200s alternate infinite
+    pointer-events: none
+
+    &::before,
+    &::after
+      background: red
+      content: ''
+      position: absolute
+      width: 100%
+      height: 100%
+      border-radius: 50%
+      transform-origin: -10vw
+
+    &::before
+      background: black
+      opacity: 0.4
+      animation: drift ease alternate infinite
+
+    &::after
+      background: white
+      opacity: 0
+      box-shadow: 0 0 0vw 0vw white
+      animation: drift ease alternate infinite, flash ease infinite
+
+  @for $i from 1 through $quantity
+
+    $steps: random(12) + 16
+    $rotationSpeed: random(10) + 8s
+
+    .firefly:nth-child(#{$i})
+      animation-name: move#{$i}
+
+      &::before
+        animation-duration: #{$rotationSpeed}
+
+      &::after
+        animation-duration: #{$rotationSpeed}, random(6000) + 5000ms
+        animation-delay: 0ms, random(8000) + 500ms
+
+    @keyframes move#{$i}
+      @for $step from 0 through $steps
+        #{$step * math.div(100 , $steps)}%
+          transform: translateX(random(100) - 50vw) translateY(random(100) - 50vh) scale(math.div(random(75) , 100) + .25)
+
+  @keyframes drift
+    0%
+      transform: rotate(0deg)
+    100%
+      transform: rotate(360deg)
+
+  @keyframes flash
+    0%, 30%, 100%
+      opacity: 0
+      box-shadow: 0 0 0vw 0vw white
+    5%
+      opacity: 1
+      box-shadow: 0 0 2vw 0.4vw white
+</style>
